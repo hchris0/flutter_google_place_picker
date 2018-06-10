79f16682535ddcdfa606f11c0a3e7358063b4a58
hchris0 <chrihatz@gmail.com>
2018-06-10 16:23:40 +0000

    Changed return type to dynamic because it was crashing on Android.

--- lib/flutter_google_place_picker.dart
+++ lib/flutter_google_place_picker.dart
@@ -6,6 +6,6 @@ class FlutterGooglePlacePicker {
   static const MethodChannel _channel =
       const MethodChannel('flutter_google_place_picker');
       
-  static Future<Map> get launchPlacePicker =>
+  static Future<dynamic> get launchPlacePicker =>
       _channel.invokeMethod('launchplacepicker');
 }
